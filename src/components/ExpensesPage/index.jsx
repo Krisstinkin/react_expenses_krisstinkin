af527792185d8b4351134b45409f8d005b2daf9b
Krisstinkin <krisstinkin@mail.ru>
2023-03-26 13:34:50 +0300

    начинаю делать доходы. коммит если что-то сломаю

--- src/components/ExpensesPage/index.jsx
+++ src/components/ExpensesPage/index.jsx
@@ -51,52 +51,54 @@ const ExpensesPage = () => {
 
     return (
         <div className="bg-slate-100">
-            
+
             <div className="max-w-screen-lg mx-auto px-6 md:px-10 py-16 text-center text-amber-600">
                 <Link to={'/'} className="hover:underline text-slate-500 font-semibold text-sm flex justify-left">Назад</Link>
                 <h1 className="text-black font-bold text-4xl items-center mb-4">Расходы</h1>
                 <div class="underline h-0.5 w-200 bg-amber-300 mr-auto ml-auto mb-12"></div>
+
+                <div className="flex"> 
+
+                    <div className="flex ">
+                        <Diagram data={data} />
+                    </div>
+                            
+                    <div className="flex flex-col">
+
+                        {categories.map(category => (
+                            <div>
+                                <p className="text-lg text-slate-500 ">{category}</p>
+                                <p className="text-lg text-slate-500 font-bold"> {new Intl.NumberFormat('ru-RU').format(expenses.filter(expense => expense.category === category).reduce((acc, expense) => acc + Number(expense.cost), 0 ))} руб.</p>
+                            </div>
+                        
+                        ))}
+                    </div>
+                </div>   
                 
                 <Form addExpense={addExpense}/>
                 
                 <div> 
-                
                     <div className="text-stone-600 text-lg md:text-xl">
-                    <div className="bg-slate-200 text-amber-400 font-semibold rounded px-6 py-2 mb-12 flex justify-between mx-12">Всего: <span className="text-slate-500">{new Intl.NumberFormat('ru-RU').format(expenses.reduce((acc, expense) => acc + Number(expense.cost), 0))} руб</span></div>
-                    
-                        <div className="flex flex-row justify-center"> 
-                        <Diagram data={data} />
-                        {categories.map(category => (
-                          
-                        <div className="flex flex-row justify-center"> 
-                            
-                            <p className="text-lg text-slate-500 ">{category}</p>
-                            <p className="text-lg text-slate-500 font-bold"> : {new Intl.NumberFormat('ru-RU').format(expenses.filter(expense => expense.category === category).reduce((acc, expense) => acc + Number(expense.cost), 0 ))} руб.</p>
-                            
-                            
-                        </div> 
-                        
-                        ))}
-                      </div>   
-                        
+                        <div className="bg-slate-200 text-amber-400 font-semibold rounded px-6 py-2 mb-12 flex justify-between mx-12">
+                            Всего: <span className="text-slate-500">{new Intl.NumberFormat('ru-RU').format(expenses.reduce((acc, expense) => acc + Number(expense.cost), 0))} руб</span>
+                        </div>
                     </div>
                 </div>
 
                 {expenses.length === 0 && ( 
-                <div className="mt-20 text-center text-3xl text-slate-500 font-bold">У вас пока нет расходов<br/><span className="mt-10 text-center text-base text-slate-500 font-normal">Заполните форму для добавления расходов</span></div>) 
-                }
+                <div className="mt-20 text-center text-3xl text-slate-500 font-bold">У вас пока нет расходов<br/><span className="mt-10 text-center text-base text-slate-500 font-normal">Заполните форму для добавления расходов</span></div>
+                )}
 
                 {expenses.length > 0 && expenses.reverse().map((expense) => {
                     return (
-                <Expense key={expense.id} expense={expense} deleteCosts={deleteCosts} />
+                        <Expense key={expense.id} expense={expense} deleteCosts={deleteCosts} />
                     )
-            })}
+                })}
+
             </div>
+            
         </div>
     )
-
-
-
 }
 
 export default ExpensesPage
